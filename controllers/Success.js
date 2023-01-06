28dd3e6673c1de1e01d90a475f51cd780106bb9b
nitish1899 <nkword1899@gmail.com>
2023-01-06 12:27:49 +0530

    Adding product id to the path

--- controllers/Success.js
+++
@@ -1,6 +0,0 @@
-const rootDir = require('../util/path');
-const path = require('path');
-
-exports.Success = (req,res,next) => {
-    res.sendFile(path.join(rootDir, 'views', 'success.html'));
-};
--- controllers/contact.js
+++
@@ -1,6 +0,0 @@
-const rootDir = require('../util/path');
-const path = require('path');
-
-exports.contactPerson = (req,res,next) => {
-    res.sendFile(path.join(rootDir, 'views', 'Contactus.html'));
-};
