a472d77343eddb395d4f08351ba38371f1cbaee6
nitish1899 <nkword1899@gmail.com>
2023-01-08 19:46:30 +0530

    Added a cart model ang editing product data

--- models/product.js
+++ models/product.js
@@ -1,36 +1,58 @@
 const fs = require('fs');
 const path = require('path');
-const p = path.join(path.dirname(process.mainModule.filename),
-       'data',
-       'products.json'
-       );
 
-const getProductsFromFile = cb =>{
-        fs.readFile(p, (err, fileContent) =>{
-            if(err){
-             cb([]);
-            } else{
-                cb(JSON.parse(fileContent));
-            }
-        }); 
-}
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-
-module.exports = class Project {
-    constructor(t) {
-        this.title = t;
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
+  });
+};
 
-    save(){
-       getProductsFromFile(products => {
-          products.push(this);
-          fs.writeFile(p, JSON.stringify(products),(err) => {
-            console.log(err);
+module.exports = class Product {
+  constructor(id,title,price, imageUrl, description) {
+    this.id=id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id===this.id);
+        const updatedProduct = [...products];
+        updatedProduct[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+          console.log(err);
         });
-       });
-    }
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+        });
+      } 
+    });
+  }
 
-    static fetchAll(cb) {
-         getProductsFromFile(cb);  
-    }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id,cb){
+    getProductsFromFile(products =>{
+    const product = products.find(p => p.id==id); // find is default JS method 
+    cb(product);
+  });
+}
 };
